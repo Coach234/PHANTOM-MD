5bc8f0e7041d4ba654bb1cb5ec56b36414c96ef1
Abhishek Suresh <104509404+AbhishekSuresh2@users.noreply.github.com>
2023-12-22 15:11:52 +0530

    Update _plugin.js

--- plugins/_plugin.js
+++ plugins/_plugin.js
@@ -1,23 +1,23 @@
-const { pnix } = require("../lib");
+const { command, getUrl } = require("../lib");
 const got = require("got");
 const fs = require("fs");
-const { PluginDB, installPlugin } = require("../lib/database/plugins").Plugins;
-
+const { PluginDB, installPlugin } = require("../lib/database/plugins");
 pnix(
   {
-    pattern: "plugin",
+    pattern: "install ?(.*)",
     fromMe: true,
     desc: "Installs External plugins",
-    type: "owner",
   },
   async (message, match) => {
-    if (!match) return await message.sendMessage(message.jid,"_Send a plugin url_");
-
+    if (!match)
+      return await message.sendMessage(
+        "send a plugin url"
+      );
+     
     try {
-      var url = new URL(match);
-    } catch (e) {
-      console.log(e);
-      return await message.sendMessage(message.jid,"_Invalid Url_");
+      var url = new URL(Url);
+    } catch {
+      return await message.sendMessage("_Invalid Url_");
     }
 
     if (url.host === "gist.github.com") {
@@ -27,37 +27,38 @@ pnix(
       url = url.toString();
     }
     var plugin_name;
-    var { body, statusCode } = await got(url);
-    if (statusCode == 200) {
-      var comand = body.match(/(?<=pattern:) ["'](.*?)["']/);
-      plugin_name = comand[0].replace(/["']/g, "").trim().split(" ")[0];
-      if (!plugin_name) {
-        plugin_name = "__" + Math.random().toString(36).substring(8);
-      }
-      fs.writeFileSync(__dirname + "/" + plugin_name + ".js", body);
+    var response = await got(url);
+    if (response.statusCode == 200) {
+      var commands = response.body
+        .match(/(?<=pattern:)(.*)(?=\?(.*))/g)
+        .map((a) => a.trim().replace(/"|'|`/, ""));
+      plugin_name = commands[0] || plugin_name[1] ||"__" + Math.random().toString(36).substring(8);
+      
+
+      fs.writeFileSync("./plugins/" + plugin_name + ".js", response.body);
       try {
         require("./" + plugin_name);
       } catch (e) {
-        fs.unlinkSync(__dirname + "/" + plugin_name + ".js");
-        return await message.sendMessage(message.jid,"Invalid Plugin\n ```" + e + "```");
+        fs.unlinkSync("/xasena/plugins/" + plugin_name + ".js");
+        return await message.sendMessage("Invalid Plugin\n ```" + e + "```");
       }
 
       await installPlugin(url, plugin_name);
 
-      await message.sendMessage(message.jid,`_New plugin installed : ${plugin_name}_`);
+      await message.sendMessage(
+        `_New plugin installed : ${commands.join(",")}_`
+      );
     }
   }
+  
 );
-
-
-
 pnix(
-  { pattern: "listplugin", fromMe: true, desc: "plugin list", type: "owner" },
+  { pattern: "plugin", fromMe: true, desc: 'plugin list' },
   async (message, match) => {
     var mesaj = "";
     var plugins = await PluginDB.findAll();
     if (plugins.length < 1) {
-      return await message.sendMessage(message.jid,"_No external plugins installed_");
+      return await message.sendMessage("No external plugins installed");
     } else {
       plugins.map((plugin) => {
         mesaj +=
@@ -67,32 +68,23 @@ pnix(
           plugin.dataValues.url +
           "\n";
       });
-      return await message.sendMessage(message.jid,mesaj);
+      return await message.sendMessage(mesaj);
     }
   }
 );
-
-
-
 pnix(
-  {
-    pattern: "remove(?: |$)(.*)",
-    fromMe: true,
-    desc: "Remove external plugins",
-    type: "owner",
-  },
+  { pattern: "remove(?: |$)(.*)", fromMe: true, desc: 'Remove external plugins' },
   async (message, match) => {
-    if (!match) return await message.sendMessage(message.jid,"_Need a plugin name_");
-
+    if (!match) return await message.sendMessage('need a plugin name');
+    if (!match.startsWith("__")) match = "__" + match;
     var plugin = await PluginDB.findAll({ where: { name: match } });
-
     if (plugin.length < 1) {
-      return await message.sendMessage(message.jid,"_Plugin not found_");
+      return await message.sendMessage("_Plugin not found_");
     } else {
       await plugin[0].destroy();
       delete require.cache[require.resolve("./" + match + ".js")];
-      fs.unlinkSync(__dirname + "/" + match + ".js");
-      await message.sendMessage(message.jid,`Plugin ${match} deleted`);
+      fs.unlinkSync("./plugins/" + match + ".js");
+      await message.sendMessage(`Plugin ${match} deleted`);
     }
   }
 );
