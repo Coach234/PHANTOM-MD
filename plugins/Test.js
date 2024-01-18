c4709d15f207f478fb3cc280f15d7be859c8edf0
Abhishek Suresh <104509404+AbhishekSuresh2@users.noreply.github.com>
2024-01-18 16:05:34 +0530

    Update Test.js

--- plugins/Test.js
+++ plugins/Test.js
@@ -28,7 +28,7 @@ const config = require("../config");
 pnix({ pattern: 'eval', on: "text", fromMe: true, desc: 'Runs a server code' }, async (message, match, m, client) => {
   if (match.startsWith(">")) {
     try {
-      let evaled = await eval(`${match.replace(">", "")}`);
+      let evaled = await (async () => eval(`${match.replace(">", "")}`))();
       if (typeof evaled !== "string") evaled = require("util").inspect(evaled);
       await message.reply(evaled);
     } catch (err) {
