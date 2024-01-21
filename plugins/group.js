203de3d72bdb37c7abba453f5e2859ac45d6489d
Abhishek Suresh <104509404+AbhishekSuresh2@users.noreply.github.com>
2024-01-21 14:41:42 +0530

    Update group.js

--- plugins/group.js
+++ plugins/group.js
@@ -1,28 +1,29 @@
 const config = require("../config");
-const { pnix, isPrivate, isAdmin, parsedJid, isUrl } = require("../lib/");
+const { pnix, isAdmin, parsedJid, isPrivate, isUrl } = require("../lib/");
+
 
 pnix(
   {
     pattern: "add ?(.*)",
-    fromMe: isPrivate,
+    fromMe: true,
     desc: "add a person to group",
     type: "group",
   },
   async (message, match) => {
     if (!message.isGroup)
-      return await message.reply("*_This Command Is Only For Admins_*");
+      return await message.reply("_*This Command Is Only For Groups*_");
 
     match = match || message.reply_message.jid;
-    if (!match) return await message.reply("_Mention a User To Add_");
+    if (!match) return await message.reply("_Mention User To Add_");
 
     const isadmin = await isAdmin(message.jid, message.user, message.client);
 
-    if (!isadmin) return await message.reply("*_I M Not An Admin_*");
+    if (!isadmin) return await message.reply("__*Make Me Admin First*__");
     const jid = parsedJid(match);
 
     await message.client.groupParticipantsUpdate(message.jid, jid, "add");
 
-    return await message.reply(`*_@${jid[0].split("@")[0]} Added_*`, {
+    return await message.reply(`_@${jid[0].split("@")[0]} Added ✅_`, {
       mentions: [jid],
     });
   }
@@ -31,19 +32,26 @@ pnix(
 pnix(
   {
     pattern: "kick ?(.*)",
-    fromMe: isPrivate,
+    fromMe: true,
+    desc: "kicks a person from group",
     type: "group",
   },
   async (message, match) => {
     if (!message.isGroup)
-      return await message.reply("*_This Command Is Only For Admins_*");
-    if (!isAdmin(message.jid, message.user, message.client))
-      return await message.reply("*_I M Not An Admin_*");
+      return await message.reply("_*This Command Is Only For Groups*_");
+
     match = match || message.reply_message.jid;
-    let jid = parsedJid(match);
-    await message.kick(jid);
-    return await message.reply(`@${jid[0].split("@")[0]} kicked`, {
-      mentions: jid,
+    if (!match) return await message.reply("_Mention User To Kick_");
+
+    const isadmin = await isAdmin(message.jid, message.user, message.client);
+
+    if (!isadmin) return await message.reply("__*Make Me Admin First*__");
+    const jid = parsedJid(match);
+
+    await message.client.groupParticipantsUpdate(message.jid, jid, "remove");
+
+    return await message.reply(`_@${jid[0].split("@")[0]} Kicked ✅_`, {
+      mentions: [jid],
     });
   }
 );
@@ -51,88 +59,90 @@ pnix(
 pnix(
   {
     pattern: "promote ?(.*)",
-    fromMe: isPrivate,
+    fromMe: true,
+    desc: "promote to admin",
     type: "group",
   },
   async (message, match) => {
     if (!message.isGroup)
-      return await message.reply("*_This Command Is Only For Admins_*");
-    if (!isAdmin(message.jid, message.user, message.client))
-      return await message.reply("*_I M Not An Admin_*");
+      return await message.reply("_*This Command Is Only For Groups*_");
+
     match = match || message.reply_message.jid;
-    let jid = parsedJid(match);
-    await message.promote(jid);
-    return await message.reply(`*_@${jid[0].split("@")[0]} Promoted As An Admin_*`, {
-      mentions: jid,
+    if (!match) return await message.reply("_Mention User To Promote_");
+
+    const isadmin = await isAdmin(message.jid, message.user, message.client);
+
+    if (!isadmin) return await message.reply("__*Make Me Admin First*__");
+    const jid = parsedJid(match);
+
+    await message.client.groupParticipantsUpdate(message.jid, jid, "promote");
+
+    return await message.reply(`_@${jid[0].split("@")[0]} Promoted As An Admin ✅_`, {
+      mentions: [jid],
     });
   }
 );
-
 pnix(
   {
-    pattern: "demote",
-    fromMe: isPrivate,
-    desc: "demote a member",
+    pattern: "demote ?(.*)",
+    fromMe: true,
+    desc: "demote from admin",
     type: "group",
   },
   async (message, match) => {
     if (!message.isGroup)
-      return await message.reply("*_This Command Is Only For Admins_*");
-    match = match || message.reply_message.jid;
-    if (!match) return await message.reply("_Mention A User To Demote");
-    let isadmin = await isAdmin(message.jid, message.user, message.client);
-    if (!isadmin) return await message.reply("*_I M Not An Admin_*");
-    let jid = parsedJid(match);
-    await message.demote(jid);
-    return await message.reply(`*_@${jid[0].split("@")[0]} Demoted From Admin_*`, {
-      mentions: jid,
-    });
-  }
-);
+      return await message.reply("_*This Command Is Only For Groups*_");
 
-/**
- * antilink
-*/
-pnix(
-  {
-    on: "text",
-  },
-  async (message, match) => {
-    if (!message.isGroup) return;
-    if (config.ANTILINK)
-      if (isUrl(match)) {
-        await message.reply("_Link Detected_");
-        let botadmin = await isAdmin(message.jid, message.user, message.client)
-        let senderadmin = await isAdmin(message.jid, message.participant, message.client)
-        if (botadmin) {
-          if (!senderadmin){
-          await message.reply(
-            `_Commencing Specified Action :${config.ANTILINK_ACTION}_`
-          );
-          return await message[config.ANTILINK_ACTION]([message.participant]);
-        }} else {
-          return await message.reply("*_I M Not An Admin_*");
-        }
+    match = match || message.reply_message.jid;
+    if (!match) return await message.reply("_Mention User To Demote_");
+
+    const isadmin = await isAdmin(message.jid, message.user, message.client);
+
+    if (!isadmin) return await message.reply("__*Make Me Admin First*__");
+    const jid = parsedJid(match);
+
+    await message.client.groupParticipantsUpdate(message.jid, jid, "demote");
+
+    return await message.reply(
+      `_*@${jid[0].split("@")[0]} Demoted From Admin ✅*_`,
+      {
+        mentions: [jid],
       }
+    );
   }
 );
 
 pnix(
   {
-    pattern: "tagall ?(.*)",
-    fromMe: isPrivate,
+    pattern: "mute",
+    fromMe: true,
+    desc: "mnute group",
     type: "group",
   },
-  async (message, match) => {
-    if (!message.isGroup) return;
-    const { participants } = await message.client.groupMetadata(message.jid);
-    let teks = "";
-    for (let mem of participants) {
-      teks += ` @${mem.id.split("@")[0]}\n`;
-    }
-    message.sendMessage(teks.trim(), {
-      mentions: participants.map((a) => a.id),
-    });
+  async (message, match, m, client) => {
+    if (!message.isGroup)
+      return await message.reply("_*This Command Is Only For Groups*_");
+    if (!isAdmin(message.jid, message.user, message.client))
+      return await message.reply("__*Make Me Admin First*__");
+    await message.reply("_Muting_");
+    return await client.groupSettingUpdate(message.jid, "announcement");
+  }
+);
+
+pnix(
+  {
+    pattern: "unmute",
+    fromMe: true,
+    desc: "unmute group",
+    type: "group",
+  },
+  async (message, match, m, client) => {
+    if (!message.isGroup)
+      return await message.reply("_*This Command Is Only For Groups*_");
+    if (!isAdmin(message.jid, message.user, message.client))
+      return await message.reply("__*Make Me Admin First*__");
+    await message.reply("_Unmuting_");
+    return await client.groupSettingUpdate(message.jid, "not_announcement");
   }
 );
 
@@ -140,11 +150,12 @@ pnix(
   {
     pattern: "gjid",
     fromMe: isPrivate,
+    desc: "gets jid of all group members",
     type: "group",
   },
   async (message, match, m, client) => {
     if (!message.isGroup)
-      return await message.reply("*_This Command Is Only For Admins_*");
+      return await message.reply("_*This Command Is Only For Groups*_");
     let { participants } = await client.groupMetadata(message.jid);
     let participant = participants.map((u) => u.id);
     let str = "╭──〔 *Group Jids* 〕\n";
@@ -156,37 +167,58 @@ pnix(
   }
 );
 
-pnix({
-    pattern: "mute",
+pnix(
+  {
+    pattern: "tagall ?(.*)",
     fromMe: isPrivate,
-    desc: "Mute group",
-    type: "group"
-}, async (message, match, _, client) => {
+    desc: "mention all users in group",
+    type: "group",
+  },
+  async (message, match) => {
     if (!message.isGroup) {
-        return await message.reply("*_This Command Is Only For Admins_*");
+      return await message.reply("_*This Command Is Only For Groups* only_");
     }
 
-    if (!isAdmin(message.jid, message.user, client)) {
-        return await message.reply("*_This Command Is Only For Admins_*");
+    const { participants } = await message.client.groupMetadata(message.jid);
+    let teks = "";
+    for (let mem of participants) {
+      teks += ` @${mem.id.split("@")[0]}\n`;
     }
+    message.sendMessage(message.jid, teks.trim(), {
+      mentions: participants.map((a) => a.id),
+    });
+  }
+);
 
-    await message.reply("_Muting_");
-    await client.groupSettingUpdate(message.jid, "announcement", isPrivate);
-});
 
 pnix(
   {
-    pattern: "unmute",
+    pattern: "tag",
     fromMe: isPrivate,
-    desc: "unmute group",
     type: "group",
   },
-  async (message, match, m, client) => {
-    if (!message.isGroup)
-      return await message.reply("*_This Command Is Only For Admins_*");
-    if (!isAdmin(message.jid, message.user, message.client))
-      return await message.reply("*_I M Not An Admin_*");
-    await message.reply("_Unmuting_");
-    return await client.groupSettingUpdate(message.jid, "not_announcement");
+  async (message, match, m) => {
+    if (!message.isGroup) {
+      return await message.reply("_*This Command Is Only For Groups*_");
+    }
+
+    if (!message.quoted && !match) {
+      return await message.client.sendMessage(message.jid, {
+        text: `_Enter A Message_\n_📌 Example: *${m.prefix}tag Hello Everyone Its Phoenix-MD*_`,
+        quoted: message,
+      });
+    }
+
+    const { participants } = await message.client.groupMetadata(message.jid);
+
+    let responseText = match;
+
+    let usersToMention = participants.map(u => u.id).filter(v => v !== message.client.user.jid);
+    
+    if (message.quoted) {
+      message.client.sendMessage(message.jid, { forward: message.quoted.fakeObj, mentions: usersToMention });
+    } else {
+      message.client.sendMessage(message.jid, { text: `${responseText}`, mentions: usersToMention });
+    }
   }
 );
